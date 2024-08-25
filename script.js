2e4ad1387e98254ebc3e30582c94db137477ea1b
q1u4a0n9 <thaianhquan20q05u09a14n@gmail.com>
2024-08-25 18:49:48 +0700

    script.js

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function changeImage() {
 
     // Mapping between quality and image file name
     const qualityImageMap = {
-        '144p': 'images/144p (1).png',
-        '240p': 'images/240p.png',
-        '480p': 'images/480p.png',
-        '720p': 'images/720p.png',
-        '1080p': 'images/1080p.png'
+        '144p': '144p (1).png',
+        '240p': '240p.png',
+        '480p': '480p.png',
+        '720p': '720p.png',
+        '1080p': '1080p.png'
     };
 
     // Change image source based on selected quality
